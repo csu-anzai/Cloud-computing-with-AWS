a7ae2d96e5699e89c83f9bb903fa60340866085e
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-26 18:51:43 -0400

    reframe command and space after h

--- webapp/cdScripts/afterInstall.sh
+++ webapp/cdScripts/afterInstall.sh
@@ -1,2 +1,2 @@
-echo "show databases" | mysql -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD -h$RDS_INSTANCE
+echo "show databases" | mysql -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD -h $RDS_INSTANCE
 #< /home/centos/webapp/cdScripts/createScripts.sql

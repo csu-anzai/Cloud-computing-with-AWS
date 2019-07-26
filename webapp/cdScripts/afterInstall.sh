166775a9fec9b36c26c94bd25f0516958f90b517
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-26 18:23:19 -0400

    home centos path for sql script

--- webapp/cdScripts/afterInstall.sh
+++ webapp/cdScripts/afterInstall.sh
@@ -1,1 +1,1 @@
-mysql -h$RDS_INSTANCE -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD < /webapp/cdScripts/createScripts.sql
+mysql -h$RDS_INSTANCE -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD < /home/centos/webapp/cdScripts/createScripts.sql

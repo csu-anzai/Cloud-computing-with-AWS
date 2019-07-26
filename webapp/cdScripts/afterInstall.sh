9f196dbebd038420012c021c526e074cf57516c1
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-26 18:31:54 -0400

    password 2 times

--- webapp/cdScripts/afterInstall.sh
+++ webapp/cdScripts/afterInstall.sh
@@ -1,2 +1,1 @@
-mysql -h$RDS_INSTANCE -p$MYSQL_DATABASE_PASSWORD < /home/centos/webapp/cdScripts/createScripts.sql
-#-u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD < /home/centos/webapp/cdScripts/createScripts.sql
+mysql -h$RDS_INSTANCE -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD -p$MYSQL_DATABASE_PASSWORD < /home/centos/webapp/cdScripts/createScripts.sql

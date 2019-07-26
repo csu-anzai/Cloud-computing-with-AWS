288881cceb6e80b21d22f414bdf1a1915a87f2ad
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-26 19:22:00 -0400

    rds instance

--- webapp/cdScripts/afterInstall.sh
+++ webapp/cdScripts/afterInstall.sh
@@ -1,5 +1,5 @@
-
-sudo mysql -h$RDS_INSTANCE -u $MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD < /home/centos/webapp/cdScripts/createScripts.sql
+echo $RDS_INSTANCE
+sudo mysql -h$RDS_INSTANCE -u $MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD --databse csye6225 < /home/centos/webapp/cdScripts/createScripts.sql
 
 #echo "show databases" | mysql -u $MYSQL_DATABASE_USER -h$RDS_INSTANCE -p $MYSQL_DATABASE_PASSWORD
 #< /home/centos/webapp/cdScripts/createScripts.sql

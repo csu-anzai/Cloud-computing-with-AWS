e32deec0bc18f24b9f251609d02899b040464f23
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-26 18:56:20 -0400

    added h spaces

--- webapp/cdScripts/afterInstall.sh
+++ webapp/cdScripts/afterInstall.sh
@@ -1,2 +1,2 @@
-echo "show databases" | mysql -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD
+echo "show databases" | mysql -u $MYSQL_DATABASE_USER -p $MYSQL_DATABASE_PASSWORD -h $RDS_INSTANCE
 #< /home/centos/webapp/cdScripts/createScripts.sql

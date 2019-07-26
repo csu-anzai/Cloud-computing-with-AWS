13afc17608c18f2efc4e05c3e0902bb20f582e61
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-26 19:25:54 -0400

    database new

--- webapp/cdScripts/afterInstall.sh
+++ webapp/cdScripts/afterInstall.sh
@@ -1,6 +1,6 @@
 echo $RDS_INSTANCE
 echo $REGION
-sudo mysql -h$RDS_INSTANCE -u $MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD --database csye6225 < /home/centos/webapp/cdScripts/createScripts.sql
+sudo mysql -h$RDS_INSTANCE -u$MYSQL_DATABASE_USER --database csye6225 -p$MYSQL_DATABASE_PASSWORD < /home/centos/webapp/cdScripts/createScripts.sql
 
 #echo "show databases" | mysql -u $MYSQL_DATABASE_USER -h$RDS_INSTANCE -p $MYSQL_DATABASE_PASSWORD
 #< /home/centos/webapp/cdScripts/createScripts.sql

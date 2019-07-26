af04fdea19030d17def472d41d9a087785948dc2
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-26 18:43:26 -0400

    echo mysql

--- webapp/cdScripts/afterInstall.sh
+++ webapp/cdScripts/afterInstall.sh
@@ -1,1 +1,2 @@
-mysql -h$RDS_INSTANCE -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD -p$MYSQL_DATABASE_PASSWORD < /home/centos/webapp/cdScripts/createScripts.sql
+echo "show databases" | mysql -h$RDS_INSTANCE -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD
+#< /home/centos/webapp/cdScripts/createScripts.sql

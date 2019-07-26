3421eb936f36dd01322e0d1f172177da5a8d7455
anu <thawali.a@husky.neu.edu>
2019-07-26 18:40:03 -0400

    test5

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,1 +1,1 @@
-sudo mysql -h $RDS_INSTANCE -P $DATABASE_PORT -u  $MYSQL_DATABASE_USER -p $MYSQL_DATABASE_PASSWORD < /home/centos/deploy/createScripts.sql
+sudo mysql -h $RDS_INSTANCE -u  $MYSQL_DATABASE_USER -p $MYSQL_DATABASE_PASSWORD < /home/centos/deploy/createScripts.sql

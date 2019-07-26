233229d329a56eab44b5fdc88d0a8093f6a16c5e
anu <thawali.a@husky.neu.edu>
2019-07-25 20:51:44 -0400

    test7

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-sudo systemctl stop tomcat.service
-
-sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
-
-sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
-
-# cleanup log files
-sudo rm -rf /opt/tomcat/logs/catalina*
-sudo rm -rf /opt/tomcat/logs/*.log
-sudo rm -rf /opt/tomcat/logs/*.txt
+mysql -h$RDS_INSTANCE -P 3306 -u$MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD < createScripts.sql

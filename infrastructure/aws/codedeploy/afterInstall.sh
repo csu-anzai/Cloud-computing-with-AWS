6e850911e2aa62716c38a6ff6b5f807cb0cbc283
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-27 17:23:25 -0400

    braces and spaces

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,6 +1,6 @@
 echo "anuja"
 echo $PATH
-echo $RDS_INSTANCE
+echo ${RDS_INSTANCE}
 
 
-sudo mysql -h $RDS_INSTANCE -u  $MYSQL_DATABASE_USER -p $MYSQL_DATABASE_PASSWORD -P 3306 < /home/centos/deploy/createScripts.sql
+sudo mysql -h${RDS_INSTANCE} -u${MYSQL_DATABASE_USER} -p${MYSQL_DATABASE_PASSWORD} < /home/centos/deploy/createScripts.sql

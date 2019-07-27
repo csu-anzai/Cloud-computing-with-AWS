701c9efaa874b222c3528f66a9de49cb15e3cef0
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-27 19:27:42 -0400

    pip3.6

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -5,6 +5,6 @@ echo ${RDS_INSTANCE}
 cd /home/centos/deploy/webapp
 virtualenv flaskEnv
 source flaskEnv/bin/activate
-pip install -r requirements.txt
+pip3.6 install -r requirements.txt
 
 sudo mysql -h${RDS_INSTANCE} -u${MYSQL_DATABASE_USER} -p${MYSQL_DATABASE_PASSWORD} < /home/centos/deploy/createScripts.sql

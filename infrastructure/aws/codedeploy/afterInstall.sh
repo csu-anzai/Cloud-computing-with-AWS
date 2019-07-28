8d610b294b1a21dcf05d80ffe576fc1116532398
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-28 09:07:19 -0400

    conf

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -6,7 +6,7 @@ eval ${RDS_INSTANCE}
 cd /home/centos/deploy/webapp
 sudo virtualenv flaskEnv
 source flaskEnv/bin/activate
-source /home/centos/deploy/infrastructure/aws/codedeploy/my.config
+source /home/centos/deploy/infrastructure/aws/codedeploy/my.conf
 echo $rds
 echo RDS has value ${rds}
 sudo mysql -h${rds} -u${MYSQL_DATABASE_USER} -p${MYSQL_DATABASE_PASSWORD} < /home/centos/deploy/createScripts.sql

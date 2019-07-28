69d7761403f9ce47ce1b1cf9ee826c38259f6e6e
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-28 11:41:31 -0400

    bucket name as parameter

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -32,7 +32,7 @@ echo "bucket name $7"
 
 #vpcid=$(aws ec2 describe-vpcs --query 'Vpcs[?Tags[?Key=='Name']|[?Value=='cloudsu2019']].VpcId')
 
-stack_create=$(aws cloudformation create-stack --template-body file://csye6225-cf-application.yaml --stack-name $4 --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=BUCKNAME, ParameterValue=$7, ParameterKey=AMI,ParameterValue=$1 ParameterKey=VPC,ParameterValue=$2 ParameterKey=AWSKEY,ParameterValue=$3 ParameterKey=Subnet1,ParameterValue=$5 ParameterKey=Subnet2,ParameterValue=$6)
+stack_create=$(aws cloudformation create-stack --template-body file://csye6225-cf-application.yaml --stack-name $4 --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=BUCKNAME,ParameterValue=$7 ParameterKey=AMI,ParameterValue=$1 ParameterKey=VPC,ParameterValue=$2 ParameterKey=AWSKEY,ParameterValue=$3 ParameterKey=Subnet1,ParameterValue=$5 ParameterKey=Subnet2,ParameterValue=$6)
 
 
 

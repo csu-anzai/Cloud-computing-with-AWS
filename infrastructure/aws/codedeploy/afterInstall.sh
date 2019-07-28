dba2b2bad31c6379e551fc579c11ef5189ed5bbd
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-28 00:40:21 -0400

    file address

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -6,6 +6,6 @@ echo ${RDS_INSTANCE}
 cd /home/centos/deploy/webapp
 sudo virtualenv flaskEnv
 source flaskEnv/bin/activate
-source my.cnf
+source /home/centos/deploy/infrastructure/aws/codedeploy/my.cnf
 echo $rds
 sudo mysql -h ${RDS_INSTANCE} -u ${MYSQL_DATABASE_USER} < /home/centos/deploy/createScripts.sql

8b9d5a1b48ee324f0d827429001544fbf6accfd1
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-27 20:02:16 -0400

    no pass

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -7,4 +7,4 @@ cd /home/centos/deploy/webapp
 sudo virtualenv flaskEnv
 source flaskEnv/bin/activate
 
-sudo mysql -h ${RDS_INSTANCE} -u ${MYSQL_DATABASE_USER} -p${MYSQL_DATABASE_PASSWORD} < /home/centos/deploy/createScripts.sql
+sudo mysql -h ${RDS_INSTANCE} -u ${MYSQL_DATABASE_USER} < /home/centos/deploy/createScripts.sql

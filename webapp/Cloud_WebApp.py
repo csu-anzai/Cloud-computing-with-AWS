eb819708d63f976434cf9a52098aabccd7159447
Adhira-Deogade <adhiradeogade@gmail.com>
2019-07-29 16:10:37 -0400

    sytax fix

--- webapp/Cloud_WebApp.py
+++ webapp/Cloud_WebApp.py
@@ -199,7 +199,7 @@ def presignedUrl( filename ):
     filename = filename
     print("filename : ", filename)
     s3_client = boto3.client('s3',
-    aws_access_key_id = AWS_ACCESS_KEY
+    aws_access_key_id = AWS_ACCESS_KEY,
         aws_secret_access_key = AWS_SECRET_ACCESS_KEY_ID,
          config=Config(signature_version='s3v4'))
     print("s3 client: ", s3_client)

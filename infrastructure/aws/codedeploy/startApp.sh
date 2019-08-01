d27834495641db1a8cee9590f7eebce2ce3aee56
Adhira-Deogade <adhiradeogade@gmail.com>
2019-08-01 12:28:53 -0400

    fetch ocnifg

--- infrastructure/aws/codedeploy/startApp.sh
+++ infrastructure/aws/codedeploy/startApp.sh
@@ -3,5 +3,6 @@ sudo cp -rf /home/centos/deploy/webapp/amazon-cloudwatch-agent.json /opt/aws/ama
 sudo yum install jq -y
 sudo touch /opt/aws/amazon-cloudwatch-agent/logs/csye6225.log
 sudo chmod 777 /opt/aws/amazon-cloudwatch-agent/logs/csye6225.log
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
 cd /home/centos/deploy/webapp/
 sudo python3.6 Cloud_WebApp.py

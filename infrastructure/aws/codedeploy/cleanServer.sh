14976dd3eae957c2a18d78820eef82f4498fe0c2
Adhira-Deogade <adhiradeogade@gmail.com>
2019-08-04 09:11:32 -0400

    clean server

---
+++ infrastructure/aws/codedeploy/cleanServer.sh
@@ -0,0 +1,4 @@
+sudo chown centos:centos /home/centos/webapp/*
+sudo rm -rf /home/centos/webapp
+sudo rm -rf /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.toml
+sudo rm -rf /opt/aws/amazon-cloudwatch-agent/logs/csye6225.log

fbf1e1a4819c06a07057ee89aabae560d88718d9
Adhira-Deogade <adhiradeogade@gmail.com>
2019-08-04 09:15:36 -0400

    pkill

--- infrastructure/aws/codedeploy/cleanServer.sh
+++ infrastructure/aws/codedeploy/cleanServer.sh
@@ -1,3 +1,5 @@
+
+sudo pkill -9 python3.6
 sudo chown centos:centos /home/centos/webapp/*
 sudo rm -rf /home/centos/webapp
 sudo rm -rf /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.toml

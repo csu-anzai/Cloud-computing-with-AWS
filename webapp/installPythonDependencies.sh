9cb05feef8e4e7cffbfe1ce98108695fbcae75f9
Adhira-Deogade <adhiradeogade@gmail.com>
2019-08-10 01:27:39 -0400

    uwsgi flask

--- webapp/installPythonDependencies.sh
+++ webapp/installPythonDependencies.sh
@@ -10,12 +10,10 @@ sudo setsebool -P httpd_can_network_connect true
 
 sudo scl enable rh-python36 "virtualenv /home/centos/deploy/ccwebappenv"
 
-sudo scl enable rh-python36 "source /home/centos/deploy/ccwebappenv/bin/activate"
-sudo scl enable rh-python36 "pip install -r /home/centos/deploy/requirements.txt" 
-
-sudo chown centos:centos /home/centos/deploy/ccwebappenv/
-sudo chown centos:centos /home/centos/deploy/ccwebappenv/*
-sudo scl enable rh-python36 "source /home/centos/deploy/ccwebappenv/bin/activate"
+sudo scl enable rh-python36 "source /home/centos/deploy/ccwebappenv/bin/activate && sudo pip install uwsgi flask"
 sudo scl enable rh-python36 "pip install uwsgi flask"
 sudo scl enable rh-python36 "pip install Flask_Ext"
 sudo scl enable rh-python36 "pip install mysql-connector-python"
+sudo scl enable rh-python36 "pip install -r /home/centos/deploy/requirements.txt" 
+sudo chown centos:centos /home/centos/deploy/ccwebappenv/*
+sudo chown centos:centos /home/centos/deploy/ccwebappenv/

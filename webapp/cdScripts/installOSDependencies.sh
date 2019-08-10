699fdcb040014d409b49435f1febdb9d1344a257
Adhira-Deogade <adhiradeogade@gmail.com>
2019-08-09 20:08:42 -0400

    scl

--- webapp/cdScripts/installOSDependencies.sh
+++ webapp/cdScripts/installOSDependencies.sh
@@ -13,3 +13,10 @@ sudo yum install jq -y
 sudo touch /opt/aws/amazon-cloudwatch-agent/logs/csye6225.log
 sudo chmod 777 /opt/aws/amazon-cloudwatch-agent/logs/csye6225.log
 
+
+sudo yum -y install centos-release-scl
+sudo yum-config-manager --enable centos-sclo-rh-testing
+sudo yum -y install rh-python36
+sudo yum -y install epel-release
+sudo yum -y install python-pip python-devel gcc nginx
+sudo pip install virtualenv
--- webapp/cdScripts/installPythonDependencies.sh
+++ webapp/cdScripts/installPythonDependencies.sh
@@ -3,13 +3,13 @@ sudo chown centos:centos /home/centos/deploy/webapp
 sudo chown -R centos:centos /home/centos/deploy/webapp/*
 
 sudo semanage permissive -a httpd_t
-virtualenv /home/centos/deploy/webapp/ccwebappenv
+sudo scl enable rh-python36 "virtualenv /home/centos/deploy/webapp/ccwebappenv"
 
-source /home/centos/deploy/webapp/ccwebappenv/bin/activate
-pip install -r /home/centos/deploy/webapp/requirements.txt 
+sudo scl enable rh-python36 "source /home/centos/deploy/webapp/ccwebappenv/bin/activate"
+sudo scl enable rh-python36 "pip install -r /home/centos/deploy/webapp/requirements.txt" 
 
 sudo chown centos:centos /home/centos/webapp/ccwebappenv/
 sudo chown centos:centos /home/centos/webapp/ccwebappenv/*
-source /home/centos/deploy/webapp/ccwebappenv/bin/activate
-pip install uwsgi flask
-pip insatll Flask_Ext
+sudo scl enable rh-python36 "source /home/centos/deploy/webapp/ccwebappenv/bin/activate"
+sudo scl enable rh-python36 "pip install uwsgi flask"
+sudo scl enable rh-python36 "pip insatll Flask_Ext"

16795d9adb0d7a88635ea0e5b739339e10cb338c
Adhira-Deogade <adhiradeogade@gmail.com>
2019-08-10 01:41:37 -0400

    seq of venv

--- webapp/installPythonDependencies.sh
+++ webapp/installPythonDependencies.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
+
+sudo scl enable rh-python36 "virtualenv /home/centos/deploy/ccwebappenv"
 sudo chown centos:centos /home/centos/deploy
 sudo chown -R centos:centos /home/centos/deploy/*
 
 sudo semanage permissive -a httpd_t
 sudo setsebool -P httpd_can_network_connect true
 
-
-
-
-sudo scl enable rh-python36 "virtualenv /home/centos/deploy/ccwebappenv"
-
-sudo scl enable rh-python36 "source /home/centos/deploy/ccwebappenv/bin/activate && sudo pip install uwsgi flask"
-sudo scl enable rh-python36 "pip install uwsgi flask"
+sudo scl enable rh-python36 "source /home/centos/deploy/ccwebappenv/bin/activate && pip install uwsgi flask"
 sudo scl enable rh-python36 "pip install Flask_Ext"
 sudo scl enable rh-python36 "pip install mysql-connector-python"
 sudo scl enable rh-python36 "pip install -r /home/centos/deploy/requirements.txt" 
+sudo chown centos:centos /home/centos/deploy/ccwebappenv
 sudo chown centos:centos /home/centos/deploy/ccwebappenv/*
-sudo chown centos:centos /home/centos/deploy/ccwebappenv/
